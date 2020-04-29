e1d4d63f9bce42674557133e704862ea7b838d86
ihandouk <61102791+ihandouk@users.noreply.github.com>
2020-04-29 03:25:57 -0400

    Fixed a Timer error

--- src/main/java/FarmerJack.java
+++ src/main/java/FarmerJack.java
@@ -5,6 +5,8 @@ import main.java.patterns.factory.Animals.AnimalFarm;
 import main.java.patterns.factory.Crops.CropFarm;
 import main.java.patterns.factory.Fish.FishFarm;
 import main.java.patterns.Mediator.TimeCycle;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class FarmerJack {
 
@@ -14,29 +16,36 @@ public class FarmerJack {
      */
     public static void main(String[] args) {
         
-        boolean end = false;
+        Timer t;
         
         System.out.println("WELCOME TO FARMER JACK'S FARMS \n");
-        init();
-    }
-    
-    public static void init() {
         
         System.out.println("Each farm starts with: "+ Currency.currency+ " Dollars\n");
         System.out.println("Creating a new farm: ");
         AnimalFarm.buildFarm build = new AnimalFarm.buildFarm();
         FishFarm.buildFarm build1 = new FishFarm.buildFarm();
         CropFarm.buildFarm build2 = new CropFarm.buildFarm();
+        t = new Timer();
         
         AnimalFarm farm = build.create();
         Currency.animalList.add(farm);
-        FishFarm farm1 = build1.create();
-        Currency.fishList.add(farm1);
-        CropFarm farm2 = build2.create();
-        Currency.cropList.add(farm2);
+        AnimalFarm farm1 = build.create();
+        Currency.animalList.add(farm1);
+        AnimalFarm farm2 = build.create();
+        Currency.animalList.add(farm2);
+        FishFarm farm3 = build1.create();
+        Currency.fishList.add(farm3);
+        FishFarm farm4 = build1.create();
+        Currency.fishList.add(farm4);
+        FishFarm farm5 = build1.create();
+        Currency.fishList.add(farm5);
+        CropFarm farm6 = build2.create();
+        Currency.cropList.add(farm6);
+        CropFarm farm7 = build2.create();
+        Currency.cropList.add(farm7);
+        CropFarm farm8 = build2.create();
+        Currency.cropList.add(farm8);
         new TimeCycle(3);
-        
-        
         
     }
 
--- src/main/java/patterns/Mediator/TimeCycle.java
+++ src/main/java/patterns/Mediator/TimeCycle.java
@@ -3,47 +3,40 @@ package main.java.patterns.Mediator;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import main.java.patterns.factory.Currency;
 import main.java.patterns.factory.LifeCycle;
 
 public class TimeCycle {
     
-    private Timer cycle;
+    static Timer timer;
     private static boolean isDay = false;
     private static int sunRise = 0;
     
+    
     public TimeCycle(int seconds) {
         
-        cycle = new Timer();
+        timer = new Timer();
         
         if(isDay) {
-            cycle.schedule(new Midnight(), seconds * 300);
+            timer.schedule(new Midnight(), seconds * 300);
         }else {
-            cycle.schedule(new FiveAM(), seconds * 300);
+            timer.schedule(new FiveAM(), seconds * 300);
         }
+        
     }
     
-    private void endSim(String message) {
-        this.cycle.cancel();
-        this.isDay = false;
-    }
-    
-    private void simCheck() {
-        if(Currency.currency >= 2000) {
-            endSim("End of simulation, farms are at capacity");
-            return;
-        }
-    }
     
     class FiveAM extends TimerTask{
         
         public void run() {
+            
             System.out.println("The early bird gets the worm Y'LL!");
             isDay = true;
             sunRise++;
             System.out.println("It is a new day: "+ sunRise);
             new TimeCycle(3);
             new LifeCycle();
+            
+            
         }
     }
     
@@ -53,6 +46,11 @@ public class TimeCycle {
             System.out.println("The sun is settin Y'LL, tomorrow we will be here again bright and early! \n");
             isDay = false;
             new TimeCycle(3);
+            if(sunRise == 10) {
+                timer.cancel();
+                System.out.println("End of simulation.");
+            }
+            
         }
     }
 }
