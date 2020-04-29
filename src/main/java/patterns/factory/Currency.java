5357f2c4102f9b4cbadceab98b52ca14f40770dc
ihandouk <61102791+ihandouk@users.noreply.github.com>
2020-04-29 05:46:09 -0400

    Update Currency.java

--- src/main/java/patterns/factory/Currency.java
+++ src/main/java/patterns/factory/Currency.java
@@ -9,9 +9,42 @@ import main.java.patterns.factory.Fish.FishFarm;
 
 public class Currency {
     
-    public static int currency = 300;
+    public static int currency = 200;
+    public static int readyUpgrade = 1200;
     public static List<AnimalFarm> animalList = new ArrayList<AnimalFarm>();
     public static List<FishFarm> fishList = new ArrayList<FishFarm>();
     public static List<CropFarm> cropList = new ArrayList<CropFarm>();
+    
+    public static List<AnimalFarm> getAnimalFarmList(){
+        return animalList;
+    }
+    
+    public static List<FishFarm> getFishFarmList(){
+        return fishList;
+    }
+    
+    public static List<CropFarm> getCropFarmList(){
+        return cropList;
+    }
+    
+    public static int getCurrency() {
+        return currency;
+    }
+    
+    public static int getreadyUpgrade() {
+        return readyUpgrade;
+    }
+    
+    public static void addCurrency(int newCurrency) {
+        currency = newCurrency;
+    }
+    
+    public static void setCurrency(int newCurrency) {
+        currency = newCurrency;
+    }
+    
+    public static void setreadyUpgrade(int Upgrade) {
+        currency = Upgrade;
+    }
 
 }
